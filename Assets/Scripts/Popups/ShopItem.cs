6c7c5233bd47992a63c0ece461b8003f24ebb38f
Hussain Shauzab Abis <hussain0305@users.noreply.github.com>
2024-11-23 17:17:27 +0200

    UI Glow

--- Assets/Scripts/Popups/ShopItem.cs
+++ Assets/Scripts/Popups/ShopItem.cs
@@ -4,7 +4,6 @@ public class ShopItem : MenuItem<PurchasableItemInfo>
     {
         var purchasableItem = (PurchasableItemInfo)(object)itemData;
         int playerCoins = GameManager.Instance.PlayerInventory.GetCoinsBalance();
-        gameObject.name = "processed";
         //Inventory section will be active if it's both available and player has enough coins to buy it
         inventorySection.SetActive(purchasableItem.inventoryCount > 0 && playerCoins >= purchasableItem.price);
         //sold out section will be active if it's out of stock
